c0ea8df1b2c1882feef4a5f9bdeec9bd3a8dddcf
YashMunjal <yashmu45@gmail.com>
2020-04-27 20:55:15 +0530

    disastrous commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import ProfileCard from './components/profileCard'
 import './App.css';
 
 function App() {
+  //Set states
+  const [profile, setProfile] = useState([]);
+  const [search, setSearch] = useState('');
+  const [profilequery, setQuery] = useState('');
+
+  //api endpoints
+  const profileRequest = `https://api.github.com/users/yashmunjal`
+
+  //Get profiles
+
+  const getProfiles = async () => {
+    const response = await fetch(profileRequest);
+    const data = await response.json();
+    setProfile(data);
+    console.log(data);
+  }
+  //getting profile
+  useEffect(() => {
+    getProfiles();
+  },[]);
+
+
+  //update search
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+
+  
+  //Get search
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  //Main app
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form className="search-form" onSubmit={getSearch}>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <ProfileCard ></ProfileCard>
     </div>
   );
 }
---
+++ src/components/profileCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+const profileCard = () => {
+    return (
+        <div className="profile-card">
+            <div className="profile-pic"></div>
+        </div>
+    );
+}
+export default profileCard;
