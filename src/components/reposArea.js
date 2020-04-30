a1a597fa7ac3d9fcc75e24490790e80047ad69d5
YashMunjal <yashmu45@gmail.com>
2020-05-01 04:08:54 +0530

    language part left

--- src/components/reposArea.js
+++ src/components/reposArea.js
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from 'react';
 import '../App.css';
 import axios from 'axios';
 
-const Repos = ({ name, refresh, id, secret }) => {
+const Repos = ({ name, refresh }) => {
     const showHide = () => {
+        if(refresh===0){
+            document.querySelector('ul').style.display='none' ;
+        }
+        else{
+            document.querySelector('ul').style.display='block';
+        }
     }
 
-
-
-    const reposLink = `https://api.github.com/users/${name}/repos?client_id=${id}&client_secret=${secret}&per_page=20&page=1`
+    const [counter,setcounter]=useState(1);
+    const reposLink = `https://api.github.com/users/${name}/repos?per_page=10&page=${counter}`
     const [repos, setrepos] = useState([]);
 
     const showRepos = async () => {
@@ -18,45 +23,29 @@ const Repos = ({ name, refresh, id, secret }) => {
             },
         });
         setrepos(response.data);
-        console.log(response.data);
+        console.log(response);
     }
 
     useEffect(() => {
         showHide(); //children function of interest
         showRepos();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [refresh]);
+    }, [refresh,name]);
 
     return (
         <div className="repo-area">
             <ul className="repos-list">
-                <li className="cards_item">
+                {repos.map(repo=>(
+                    <li key={repo.id} className="cards_item">
                     <div className="card">
                         <div className="card_content">
-                            <h2 className="card_title">Card Grid Layout</h2>
-                            <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-                            <button className="btn card_btn">Read More</button>
+                            <h2 className="card_title">{repo.name}</h2>
+                            <p className="card_text">{repo.description}</p>
+                            <button className="btn card_btn">Show language stats</button>
                         </div>
                     </div>
                 </li>
-                <li className="cards_item">
-                    <div className="card">
-                        <div className="card_content">
-                            <h2 className="card_title">Card Grid Layout</h2>
-                            <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-                            <button className="btn card_btn">Read More</button>
-                        </div>
-                    </div>
-                </li>
-                <li className="cards_item">
-                    <div className="card">
-                        <div className="card_content">
-                            <h2 className="card_title">Card Grid Layout</h2>
-                            <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
-                            <button className="btn card_btn">Read More</button>
-                        </div>
-                    </div>
-                </li>
+                ))}
             </ul>
         </div>
     );
